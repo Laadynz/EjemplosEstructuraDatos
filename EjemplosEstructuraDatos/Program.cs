b06e1dde3c6db13bda7542b1a5f0906965a4e5be
LadyNunez <laadynz@hotmail.com>
2022-03-30 21:37:29 -0700

    Agregamos nuevo alumno 3

--- EjemplosEstructuraDatos/Program.cs
+++ EjemplosEstructuraDatos/Program.cs
@@ -6,17 +6,26 @@ Alumno alumno1 = new Alumno();
 alumno1.Nombres = "Antonio";
 alumno1.ApellidoPaterno = "Rogel";
 alumno1.Matricula = "123";
-alumno1.FechaNacimiento = DateTime.Now;
+alumno1.FechaNacimiento = new DateTime(1985,03,05);
 
 
 Alumno alumno2 = new Alumno();
-alumno2.Nombres = "Juan";
-alumno2.ApellidoPaterno = "Lopez";
-alumno2.Matricula = "456";
-alumno2.FechaNacimiento = new DateTime(1980,02,28);
+alumno2.Nombres = "Lady";
+alumno2.ApellidoPaterno = "Nuñez";
+alumno2.Matricula = "2130050";
+alumno2.FechaNacimiento = new DateTime(1988,12,21);
+
+
+Alumno alumno3 = new Alumno();
+alumno3.Nombres = "Eyva Lucia";
+alumno3.ApellidoPaterno = "Rogel";
+alumno3.ApellidoMaterno = "Nuñez";
+alumno3.Matricula = "123456";
+alumno3.FechaNacimiento = new DateTime(2017,08,21);
 
 lista.Add(alumno1);
 lista.Add(alumno2);
+lista.Add(alumno3);
 
 foreach (Alumno alumnoEnLista in lista) 
 {
